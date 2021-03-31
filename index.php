aab09d6d96b8ce6008d38d1a865ffe58ae1e1ae2
NALLAKANNU <81615983+NALLAKANNU@users.noreply.github.com>
2021-03-31 18:36:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>eNoah News</title>
  </head>
  <body>
- <?php echo '<p>CI/CD process done with PHP</p>'; ?> 
+ <?php echo '<p>CI/CD process done with PHP and pending for webhook</p>'; ?> 
  </body>
 </html>
