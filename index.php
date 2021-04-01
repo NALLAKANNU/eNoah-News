da0277c4246bddfeedd6dbca253aa352ea58bfbc
NALLAKANNU <81615983+NALLAKANNU@users.noreply.github.com>
2021-04-01 16:04:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,3 @@
  <?php echo '<p>DEEPACK AND NALLAKANNU IS GOOD FRIENDS</p>
  </body>
 </html>
-
-node {
-    stage("composer_install") {
-        // Run `composer update` as a shell script
-        sh 'composer install'
-    }
-    stage("phpunit") {
-        // Run PHPUnit
-        sh 'vendor/bin/phpunit'
-    }
-}
